4fcaa3bdedc1071aa2cba273487b338adfb75dd4
gudehe <gudehe@huawei.com>
2021-12-16 20:03:47 +0800

    bugfix:fix class member function
    Signed-off-by:gudehe<gudehe@huawei.com>

--- services/storage_daemon/ipc/src/storage_daemon.cpp
+++ services/storage_daemon/ipc/src/storage_daemon.cpp
@@ -19,49 +19,47 @@
 
 namespace OHOS {
 namespace StorageDaemon {
-
-// StorageDaemon::StorageDaemon() {}
-int32_t Shutdown()
+int32_t StorageDaemon::Shutdown()
 {
     return E_OK;
 }
 
-int32_t Mount(std::string volId)
+int32_t StorageDaemon::Mount(std::string volId, uint32_t flags)
 {
     return E_OK;
 }
 
-int32_t UMount(std::string volId)
+int32_t StorageDaemon::UMount(std::string volId)
 {
     return E_OK;
 }
 
-int32_t Check(std::string volId)
+int32_t StorageDaemon::Check(std::string volId)
 {
     return E_OK;
 }
 
-int32_t Format(std::string voldId)
+int32_t StorageDaemon::Format(std::string voldId)
 {
     return E_OK;
 }
 
-int32_t AddUser(int32_t userId)
+int32_t StorageDaemon::AddUser(int32_t userId)
 {
     return UserManager::Instance()->AddUser(userId);
 }
 
-int32_t RemoveUser(int32_t userId)
+int32_t StorageDaemon::RemoveUser(int32_t userId)
 {
     return UserManager::Instance()->RemoveUser(userId);
 }
 
-int32_t PrepareUserDirs(int32_t userId, uint32_t flags)
+int32_t StorageDaemon::PrepareUserDirs(int32_t userId, uint32_t flags)
 {
     return UserManager::Instance()->PrepareUserDirs(userId, flags);
 }
 
-int32_t DestroyUserDirs(int32_t userId, uint32_t flags)
+int32_t StorageDaemon::DestroyUserDirs(int32_t userId, uint32_t flags)
 {
     return UserManager::Instance()->DestroyUserDirs(userId, flags);
 }
