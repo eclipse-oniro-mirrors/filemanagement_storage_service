2edd0be160c8f9e6be5f450dead5412935fb92c7
张文迪 <zhangwendi3@huawei.com>
2022-01-11 20:51:53 +0800

    judge and handle corner case
    Signed-off-by: 张文迪 <zhangwendi3@huawei.com>

--- services/storage_manager/user/src/multi_user_manager_service.cpp
+++ services/storage_manager/user/src/multi_user_manager_service.cpp
@@ -30,39 +30,100 @@ MultiUserManagerService::~MultiUserManagerService()
     LOGI("DEBUG ~MultiUserManagerService destructer ~");    
 }
 
-int32_t MultiUserManagerService::PrepareAddUser(int32_t userId) const
+int32_t MultiUserManagerService::CheckUserState(int32_t userId, UserState state)
+{
+    if (userId < 0) {
+        LOGE("MultiUserManagerService: userId:%{public}d is negative number", userId);
+        return E_USER_STATE;
+    }
+    auto iterator = users_.find(userId);
+    if (iterator == users_.end()) {
+        LOGE("MultiUserManagerService: the user %{public}d doesn't exist", userId);
+        return E_NON_EXIST;
+    }
+
+    UserInfo& user = iterator->second;
+    if (user.GetState() != state) {
+        LOGE("MultiUserManagerService: the user's state %{public}d is invalid", user.GetState());
+        return E_USER_STATE;
+    }
+
+    return E_OK;
+}
+
+inline void MultiUserManagerService::SetUserState(int32_t userId, UserState state)
+{
+    users_.at(userId).SetState(state);
+}
+
+int32_t MultiUserManagerService::PrepareAddUser(int32_t userId)
 {
     LOGI("MultiUserManagerService::PrepareAddUser, userId:%{public}d", userId);
+    if (userId < 0) {
+        LOGE("MultiUserManagerService::PrepareAddUser, userId:%{public}d is negative number", userId);
+        return E_USER_STATE;
+    }
+    auto iterator = users_.find(userId);
+    if (iterator != users_.end()) {
+        LOGE("MultiUserManagerService: the user %{public}d already exists", userId);
+        return E_EXIST;
+    }
     std::shared_ptr<StorageDaemonCommunication> sdCommunication;
     sdCommunication = DelayedSingleton<StorageDaemonCommunication>::GetInstance();
     int32_t err = sdCommunication->PrepareAddUser(userId);
+    users_.insert({ userId, UserInfo(userId, USER_CREATE)});
     return err;
 }
 
-int32_t MultiUserManagerService::RemoveUser(int32_t userId) const
+int32_t MultiUserManagerService::RemoveUser(int32_t userId)
 {
     LOGI("MultiUserManagerService::RemoveUser, userId:%{public}d", userId);
+    int32_t err = CheckUserState(userId, USER_CREATE);
+    if (err != E_OK) {
+        return err;
+    }    
     std::shared_ptr<StorageDaemonCommunication> sdCommunication;
     sdCommunication = DelayedSingleton<StorageDaemonCommunication>::GetInstance();
-    int32_t err = sdCommunication->RemoveUser(userId);
+    err = sdCommunication->RemoveUser(userId);
+    users_.erase(userId);
     return err;
 }
 
-int32_t MultiUserManagerService::PrepareStartUser(int32_t userId) const
+int32_t MultiUserManagerService::PrepareStartUser(int32_t userId)
 {
     LOGI("MultiUserManagerService::PrepareStartUser, userId:%{public}d", userId);
+    if (startedUser_ != NO_USER_STARTED) {
+        LOGE("MultiUserManagerService::PrepareStartUser, an existing user has been started");
+        return E_USER_STATE;
+    }
+    int32_t err = CheckUserState(userId, USER_CREATE);
+    if (err != E_OK) {
+        return err;
+    }
     std::shared_ptr<StorageDaemonCommunication> sdCommunication;
     sdCommunication = DelayedSingleton<StorageDaemonCommunication>::GetInstance();
-    int32_t err = sdCommunication->PrepareStartUser(userId);
+    err = sdCommunication->PrepareStartUser(userId);
+    startedUser_ = userId;
+    SetUserState(userId, USER_START);
     return err;
 }
 
-int32_t MultiUserManagerService::StopUser(int32_t userId) const
+int32_t MultiUserManagerService::StopUser(int32_t userId)
 {
     LOGI("MultiUserManagerService::StopUser, userId:%{public}d", userId);
+    if (startedUser_ == NO_USER_STARTED) {
+        LOGE("MultiUserManagerService::StopUser, no existing user has been started");
+        return E_USER_STATE;
+    }
+    int32_t err = CheckUserState(userId, USER_START);
+    if (err != E_OK) {
+        return err;
+    }
     std::shared_ptr<StorageDaemonCommunication> sdCommunication;
     sdCommunication = DelayedSingleton<StorageDaemonCommunication>::GetInstance();
-    int32_t err = sdCommunication->StopUser(userId);
+    err = sdCommunication->StopUser(userId);
+    startedUser_ = NO_USER_STARTED;
+    SetUserState(userId, USER_CREATE);
     return err;
 }
 } // StorageManager
